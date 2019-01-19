224af73b33138c44868d027e0623217ae42803a2
xNagyChannel <46797223+xNagyChannel@users.noreply.github.com>
2019-01-19 17:55:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '*'
 
 
-const developers = ["452513830270926870"]
+const developers = ["452513830270926870","465989498023706640"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
